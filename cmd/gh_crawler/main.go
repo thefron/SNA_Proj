8f2f4d0ecf11c77d072c95f680dab0058ca64c7c
Hoseong Hwang <thefron@ultracaption.net>
2016-04-01 21:54:02 +0900

    Include octokit package

--- cmd/gh_crawler/main.go
+++ cmd/gh_crawler/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/octokit/go-octokit/octokit"
+)
+
 func main() {
+	client := octokit.NewClient(nil)
+
+	url, err := octokit.UserURL.Expand(octokit.M{"user": "thefron"})
+
+	if err != nil {
+		fmt.Println("Error has occurred: ", err)
+	}
+
+	user, result := client.Users(url).One()
+	if result.HasError() {
+		// Handle error
+	}
+
+	fmt.Println(user.ReposURL)
 }
