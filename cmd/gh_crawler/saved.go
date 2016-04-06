c4afd12d0796bf5241df0a11f27c4e72741dcd10
Seulgi Kim <dev@seulgi.kim>
2016-04-06 16:05:45 +0900

    Make Saved thread safe.

--- cmd/gh_crawler/saved.go
+++ cmd/gh_crawler/saved.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"container/list"
+	"sync"
 )
 
 type Saved struct {
 	saved  map[string]bool
 	queued *list.List
+	lock   *sync.Mutex
 }
 
 func NewSaved() *Saved {
 	return &Saved{
 		make(map[string]bool),
 		list.New(),
+		&sync.Mutex{},
 	}
 }
 
 func (s *Saved) Next() *string {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	elem := s.queued.Front()
 	if elem == nil {
 		return nil
@@ -26,6 +31,8 @@ func (s *Saved) Next() *string {
 }
 
 func (s *Saved) Enqueue(candidate string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	if !s.saved[candidate] {
 		s.saved[candidate] = true
 		s.queued.PushBack(candidate)
