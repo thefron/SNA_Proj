24247b9ab547a3f35532feea6543d5033d453d79
Seulgi Kim <dev@seulgi.kim>
2016-04-27 14:48:12 +0900

    Add Event struct.

--- cmd/ga_crawler/main.go
+++ cmd/ga_crawler/main.go
@@ -106,3 +106,20 @@ func unzip(dirPath string, baseName string) error {
 
 func main() {
 }
+
+type User struct {
+	Id    uint64 `json:"id"`
+	Login string `json:"login"`
+}
+type Repo struct {
+	Id   uint64 `json:"id"`
+	Name string `json:"name"`
+}
+
+type Event struct {
+	Type      string    `json:"type"`
+	Actor     User      `json:"actor"`
+	Repo      Repo      `json:"repo"`
+	CreatedAt time.Time `json:"created_at"`
+	Org       *User     `json:"org"`
+}
