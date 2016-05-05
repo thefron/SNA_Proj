f5768479d0aadef6128478d5af049eeed50e2446
Seulgi Kim <dev@seulgi.kim>
2016-05-05 16:03:35 +0900

    Limit the number of parser to 4

--- cmd/ga_crawler/main.go
+++ cmd/ga_crawler/main.go
@@ -27,33 +27,46 @@ func main() {
 	downDir := os.Args[3]
 
 	for targetDay := end.AddDate(0, 0, -1); start.Unix() <= targetDay.Unix(); targetDay = targetDay.AddDate(0, 0, -1) {
-		err = downAndWriteEventsForDay(targetDay, downDir)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error in downAndWriteEventsForDay(%s) %s!\n", targetDay.Format(layout), err)
-		}
+		downAndWriteEventsForDay(targetDay, downDir)
 	}
 }
 
-func downAndWriteEventsForDay(date time.Time, downDir string) error {
+func downAndWriteEventsForDay(date time.Time, downDir string) {
 	c := make(chan *Event, 100)
-	year := date.Year()
-	month := date.Month()
-	day := date.Day()
+	layout := "2006-01-02"
+	fileName := path.Join(downDir, date.Format(layout))
+	go func() {
+		err := writeEvents(fileName, c)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in downAndWriteEventsForDay(%s) %s!\n", date.Format(layout), err)
+		}
+	}()
+	repeat := 4
+	for i := 0; i < 24; i += repeat {
+		collectNHour(date, i, repeat, downDir, c)
+	}
+	close(c)
+}
+
+func collectNHour(time time.Time, start int, repeat int, downDir string, c chan<- *Event) {
+	year := time.Year()
+	month := time.Month()
+	day := time.Day()
+	done := make(chan int, 1)
 	completeJob := 0
-	fileName := path.Join(downDir, fmt.Sprintf("%d-%02d-%02d", year, month, day))
-	for hour := 0; hour < 24; hour += 1 {
+	for hour := start; hour < start+repeat; hour += 1 {
 		go func(hour int) {
 			err := downAndReadEvents(year, month, day, hour, downDir, c)
 			completeJob += 1
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Error in downAndReadEvents(%d-%d-%d-%d) %s!\n", year, month, day, hour, err)
 			}
-			if completeJob == 24 {
-				close(c)
+			if completeJob == repeat {
+				done <- 1
 			}
 		}(hour)
 	}
-	return writeEvents(fileName, c)
+	<-done
 }
 
 func downAndReadEvents(year int, month time.Month, day int, hour int, downDir string, c chan<- *Event) error {
