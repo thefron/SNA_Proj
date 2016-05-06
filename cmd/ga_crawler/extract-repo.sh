b5777aa2321ab160411ce1f5468c8dbdf257a987
Seulgi Kim <dev@seulgi.kim>
2016-05-06 11:16:33 +0900

    Make extract script sort numerical.
    별 의미가 있는 건 아닌데 어쨌든 id가 적으면 옛날에 생성된 것이므로, 어딘가에 써먹을 곳이 있을 수도 있을 것 같아서

--- cmd/ga_crawler/extract-repo.sh
+++ cmd/ga_crawler/extract-repo.sh
@@ -3,6 +3,6 @@ mkdir -p ./$2
 for file in $(ls ./$1/????-??-??)
 do
     filename=$(basename $file)
-    sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\5 \4/" "$file" | sort -u > "./$2/$filename.sort"
+    sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\5 \4/" "$file" | sort -u -n > "./$2/$filename.sort"
 done
-sort -u -m ./$2/*.sort > ./$2/users
+sort -u -m -n ./$2/*.sort > ./$2/users
--- cmd/ga_crawler/extract-user.sh
+++ cmd/ga_crawler/extract-user.sh
@@ -3,6 +3,6 @@ mkdir -p ./$2
 for file in $(ls ./$1/????-??-??)
 do
     filename=$(basename $file)
-    sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\3 \2/" "$file" | sort -u > "./$2/$filename.sort"
+    sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\3 \2/" "$file" | sort -u -n > "./$2/$filename.sort"
 done
-sort -u -m ./$2/*.sort > ./$2/users
+sort -u -m -n ./$2/*.sort > ./$2/users
