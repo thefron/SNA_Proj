85c5521818d09a27e2d5edf66a55bd3ba9a34a07
Seulgi Kim <dev@seulgi.kim>
2016-05-06 11:35:10 +0900

    extract-repo.sh와 extract-org.sh가 생성하는 파일 이름 변경

--- cmd/ga_crawler/extract-org.sh
+++ cmd/ga_crawler/extract-org.sh
@@ -5,4 +5,4 @@ do
     filename=$(basename $file)
     cut -d " " -f "8,7" "$file" | grep -v "^$" | sed "s/^\([^ ]*\) \([^ ]*\)/\2 \1/" | sort -u -n > "./$2/$filename.sort"
 done
-sort -u -m -n ./$2/*.sort > ./$2/users
+sort -u -m -n ./$2/*.sort > ./$2/orgs
--- cmd/ga_crawler/extract-repo.sh
+++ cmd/ga_crawler/extract-repo.sh
@@ -5,4 +5,4 @@ do
     filename=$(basename $file)
     sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\5 \4/" "$file" | sort -u -n > "./$2/$filename.sort"
 done
-sort -u -m -n ./$2/*.sort > ./$2/users
+sort -u -m -n ./$2/*.sort > ./$2/repos
