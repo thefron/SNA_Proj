14a284137a0d6fe209b1013b77dd6888b336c060
Seulgi Kim <dev@seulgi.kim>
2016-05-06 10:56:33 +0900

    repo 정보 빼오는 스크립트 추가.
    사용법은 extract-user.sh와 동일

---
+++ cmd/ga_crawler/extract-repo.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+mkdir -p ./$2
+for file in $(ls ./$1/????-??-??)
+do
+    filename=$(basename $file)
+    sed "s/^\([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) \([^ ]*\) .*/\5 \4/" "$file" | sort -u > "./$2/$filename.sort"
+done
+sort -u -m ./$2/*.sort > ./$2/users
